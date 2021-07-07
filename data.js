765f5655cb89823239ef8f7b619e95cd6f444009
gooseisdead <gooseisdead@mac.com>
2021-07-07 17:26:55 -0400

    now

--- data.js
+++ data.js
@@ -1,177 +1,37 @@
 
-const players = [
-      {
-        "id": 1,
-        "position": "C",
-        "name": "Austin Nola",
-        "contract_value": 5,
-        "contract_length": "2021",
-        "current_team": "Yankees",
-        "40_man": true
-      },
-      {
-        "id": 2,
-        "position": "1B",
-        "name": "Pete Alonso",
-        "contract_value": 1,
-        "contract_length": "2nd year",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 3,
-        "position": "2B",
-        "name": "Wilmer Flores",
-        "contract_value": 2.2,
-        "contract_length": "2020-2022",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 4,
-        "position": "3B",
-        "name": "Brad Miller",
-        "contract_value": 0.8,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 5,
-        "position": "SS",
-        "name": "Andrelton Simmons",
-        "contract_value": 10.4,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 6,
-        "position": "OF",
-        "name": "Willie Calhoun",
-        "contract_value": 1,
-        "contract_length": "2nd year",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 7,
-        "position": "OF",
-        "name": "Teoscar Hernandez",
-        "contract_value": 1.5,
-        "contract_length": "3rd year",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 8,
-        "position": "OF",
-        "name": "Marcell Ozuna",
-        "contract_value": 16,
-        "contract_length": "2023",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 9,
-        "position": "BN",
-        "name": "Ender Inciarte",
-        "contract_value": 2.2,
-        "contract_length": "2023",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 10,
-        "position": "BN",
-        "name": "Chance Sisco",
-        "contract_value": 1,
-        "contract_length": "2nd year",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 11,
-        "position": "BN",
-        "name": "Scooter Gennett",
-        "contract_value": 22.4,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 12,
-        "position": "BN",
-        "name": "Josh Reddick",
-        "contract_value": 2,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 13,
-        "position": "BN",
-        "name": "Joe Panik",
-        "contract_value": 0.6,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 14,
-        "position": "BN",
-        "name": "Greg Garcia",
-        "contract_value": 2.5,
-        "contract_length": "5th year",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 15,
-        "position": "BN",
-        "name": "Ehire Adrianza",
-        "contract_value": 1,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 16,
-        "position": "BN",
-        "name": "Jonah Heim",
-        "contract_value": 2,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 17,
-        "position": "BN",
-        "name": "Cody Thomas",
-        "contract_value": 0.9,
-        "contract_length": "2021",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 18,
-        "position": "BN",
-        "name": "Max Stassi",
-        "contract_value": 1.5,
-        "contract_length": "3rd year",
-        "current_team": "Mets",
-        "40_man": true
-      },
-      {
-        "id": 19,
-        "position": "SP",
-        "name": "Dustin May",
-        "contract_value": 0.5,
-        "contract_length": "minors",
-        "current_team": "Mets",
-        "40_man": true
-      }
-    ]
+const cocktails = `<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div><br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>`
 
-    export { players }
-  
+export { cocktails }
