af629ef4918ab53df8090b38b5cb65c41254c119
gooseisdead <gooseisdead@mac.com>
2021-07-07 17:32:18 -0400

    fix

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-// const cocktails = `<div class="cocktail-item">
-// <h2>cocktail item</h2>
-// <p class="ings">booze, booze, booze</p>
-// <p class="menu-item-price">$12</p>
-// </div>
-// <br>
-// <div class="cocktail-item">
-// <h2>cocktail item</h2>
-// <p class="ings">booze, booze, booze</p>
-// <p class="menu-item-price">$12</p>
-// </div>
-// <br>
-// <div class="cocktail-item">
-// <h2>cocktail item</h2>
-// <p class="ings">booze, booze, booze</p>
-// <p class="menu-item-price">$12</p>
-// </div>
-// <br>
-// <div class="cocktail-item">
-// <h2>cocktail item</h2>
-// <p class="ings">booze, booze, booze</p>
-// <p class="menu-item-price">$12</p>
-// </div><br>
-// <div class="cocktail-item">
-// <h2>cocktail item</h2>
-// <p class="ings">booze, booze, booze</p>
-// <p class="menu-item-price">$12</p>
-// </div>
-// <br>
-// <div class="cocktail-item">
-// <h2>cocktail item</h2>
-// <p class="ings">booze, booze, booze</p>
-// <p class="menu-item-price">$12</p>
-// </div>`
-
+const cocktails = `<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div><br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>
+<br>
+<div class="cocktail-item">
+<h2>cocktail item</h2>
+<p class="ings">booze, booze, booze</p>
+<p class="menu-item-price">$12</p>
+</div>`
 
 
 // **********
